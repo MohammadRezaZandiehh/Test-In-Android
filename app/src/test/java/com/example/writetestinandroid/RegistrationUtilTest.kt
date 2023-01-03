15f7bcd8c32c51acf529ca961c5681173f8b72ff
mohammad <m.zandieh1999@gmail.com>
2023-01-03 15:42:09 +0330

    write the first test in test directory part 3 1401 / 10 / 13

---
+++ app/src/test/java/com/example/writetestinandroid/RegistrationUtilTest.kt
@@ -0,0 +1,72 @@
+package com.example.writetestinandroid
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+class RegistrationUtilTest {
+
+    @Test
+    fun `empty username returns false`() {
+        val result = RegistrationUtil.validateRegistrationInput(
+            "",
+            "123",
+            "123"
+        )
+        assertThat(result).isFalse()                                // we assert in this line that result is false certainly.
+    }
+
+    @Test
+    fun `valid username and correctly repeated password return true`() {
+        val result = RegistrationUtil.validateRegistrationInput(
+            "Philipp",
+            "123",
+            "123"
+        )
+        assertThat(result).isTrue()
+    }
+
+    @Test
+    fun `username already exist returns false`() {
+        val result = RegistrationUtil.validateRegistrationInput(
+            "Carl",
+            "123",
+            "123"
+        )
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `incorrectly confirm password returns false`() {
+        val result = RegistrationUtil.validateRegistrationInput(
+            "Philipp",
+            "123456",
+            "abcdefg"
+        )
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `empty password returns false`() {
+        val result = RegistrationUtil.validateRegistrationInput(
+            "Philipp",
+            "",
+            ""
+        )
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `less than digit password returns false`() {
+        val result = RegistrationUtil.validateRegistrationInput(
+            "Philipp",
+            "abcdefg5",
+            "abcdefg5"
+        )
+        assertThat(result).isFalse()
+    }
+}
+
+
+//empty password
+//password was repeated incorrectly
+//password contains less than 2 digits
